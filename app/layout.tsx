2c9cd33fd750d1caa4faf472eed8a5c4bd15b0dd
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2025-02-18 16:48:38 +0300

    analaytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Analytics } from "@vercel/analytics/react";
 // تعريف خط Poppins
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"], // الأوزان المختلفة للخط
@@ -62,6 +62,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
